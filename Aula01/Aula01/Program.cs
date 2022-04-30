cafa6f65aef973d8080909b558dfe1103ed44124
Maiconsf2 <maicon.sf2@gmail.com>
2022-04-30 01:03:05 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comentario de uma linha.
+
+            /*  
+             * Comentários de varias linhas.
+             * 
+             */
+
+
+            //Criar uma variavel.
+            // = sinal de 1 igual representa,atribuição.
+
+            string nome = "Maicon";
+            int idade = 36;
+
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.ReadKey();
+        }
+    }
+}
