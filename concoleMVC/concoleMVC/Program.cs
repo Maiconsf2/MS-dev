42d494e72a8a4b1100705bd8eb4715f0a96e5ee8
Maicon <maicon.sf2@gmail.com>
2022-05-06 21:23:09 -0300

    C# MVC Console

---
+++ concoleMVC/concoleMVC/Program.cs
@@ -0,0 +1,16 @@
+﻿using concoleMVC.Controllers;
+using concoleMVC.Models;
+using System;
+
+namespace consoleMVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProdutoController prod = new ProdutoController();
+            prod.ListarProdutos();
+
+        }
+    }
+}
